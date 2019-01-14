73ca77c999cc42f403c8404e6481892931aab2f8
Chrillaz <totalchriz@gmail.com>
2019-01-14 23:45:20 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const dotenv = require('dotenv').config({path: __dirname + '/.env'}),
+			path = require('path'),
+			webpack = require('webpack'),
+			UglifyJsPlugin = require('uglifyjs-webpack-plugin'),
+			MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = env => {
+	return {
+		entry: ['@babel/polyfill', './src/js/app.js'],
+		output: {
+			path: path.resolve(__dirname, 'dist'),
+			filename: 'bundle.min.js'
+		},
+		optimization: {
+			minimize: true,
+			minimizer: [ new UglifyJsPlugin() ]
+		},
+		devtool: 'inline-source-map',
+		module: {
+			rules: [
+				{
+					use: 'babel-loader',
+					test: /\.js$/,
+					exclude: /node_modules/
+				},
+				{
+					test: /\.s[c|a]ss$/,
+					use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+				}
+			]
+		},
+		plugins: [
+			new MiniCssExtractPlugin({
+				filename: 'style.css',
+			}),
+			new webpack.DefinePlugin({
+	      'process.env': dotenv.parsed
+	    })
+		]
+	}
+};
