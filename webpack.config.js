997c35f9f22b20c1b2c3b4341e3a88dcffd57073
Christoffer Öhman <totalchriz@gmail.com>
2019-11-10 00:47:19 +0100

    new workflow v2.0.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,71 @@
-const dotenv = require('dotenv').config({path: __dirname + '/.env'}),
-			path = require('path'),
-			webpack = require('webpack'),
+const path = require('path'),
 			UglifyJsPlugin = require('uglifyjs-webpack-plugin'),
+			OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin'),
 			MiniCssExtractPlugin = require('mini-css-extract-plugin'),
-			OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+			StyleLintPlugin = require('stylelint-webpack-plugin'),
+			BrowserSyncPlugin = require('browser-sync-webpack-plugin'),
+			SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
 
-module.exports = env => {
+module.exports = (env, argv) => {
+	const { mode } = argv,
+		projectName = process.env.PWD.split('/').pop();
+	console.log(projectName);
+
 	return {
-		entry: ['@babel/polyfill', './src/js/app.js'],
-		output: {
-			path: path.resolve(__dirname, 'dist'),
-			filename: 'bundle.min.js'
-		},
-		optimization: {
-			minimize: true,
-			minimizer: [ new UglifyJsPlugin({
-				cache: true,
-				parallel: true,
-				sourceMap: true
-			}),
-			new OptimizeCSSAssetsPlugin({}) ]
-		},
-		devtool: 'inline-source-map',
+		context: __dirname,
+		devtool: mode ? 'source-map' : false,
+		entry: './src/js/main.ts',
 		module: {
 			rules: [
 				{
-					use: 'babel-loader',
-					test: /\.js$/,
+					use: 'ts-loader',
+					test: /\.tsx?$/,
 					exclude: /node_modules/
 				},
 				{
+					test: /\.svg$/,
+					loader: 'svg-sprite-loader',
+					options: {
+						extract: true,
+						spriteFilename: 'svg-defs.svg',
+					}
+				},
+				{
 					test: /\.s[c|a]ss$/,
-					use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
-				}
+					use: [mode ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+				},
 			]
 		},
+		optimization: mode ? {} : {
+			minimize: true,
+			minimizer: [
+				new UglifyJsPlugin({
+					cache: true,
+					parallel: true,
+				}),
+				new OptimizeCSSAssetsPlugin({})
+			]
+		},
+		output: {
+			path: path.resolve(__dirname, `wp/wp-content/themes/${projectName}/assets/js`),
+			filename: '[name].min.js'
+		},
 		plugins: [
+			new SpriteLoaderPlugin(),
+			new StyleLintPlugin(),
 			new MiniCssExtractPlugin({
-				filename: 'style.css',
+				filename: '../css/[name].min.css'
 			}),
-			new webpack.DefinePlugin({
-	      'process.env': dotenv.parsed
-	    })
-		]
-	}
-};
+			new BrowserSyncPlugin({
+				files: '**/*.php',
+				injectChanges: true,
+				proxy: 'http://localhost'
+			}),
+
+		],
+		resolve: {
+			extensions: ['.tsx', '.ts', '.js']
+		},
+		watch: mode ? true : false,
+	};
+}
