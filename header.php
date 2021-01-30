059ffbf9b19bfcfa978589595d9bcbfe88c22d31
Christoffer Öhman <totalchriz@gmail.com>
2021-01-30 21:05:10 +0100

    nav menu

--- header.php
+++ header.php
@@ -12,7 +12,20 @@
     <body <?php body_class(); ?>>
   
     <?php 
-      wp_body_open(); ?>
+      wp_body_open();
+
+      if ( has_nav_menu( 'primary' ) ) {
+
+        wp_nav_menu([
+          'theme_location'       => __( 'primary', $textdomain = wp_get_theme()->get( 'TextDomain' ) ),
+          'container'            => 'nav',
+          'container_id'         => '',
+          'container_class'      => '',
+          'container_area_label' => esc_attr( 'Primary Navigation', $textdomain ),
+          'echo'                 => true
+        ]);
+      }
+    ?>
 
       <main id="main" class="main" role="main">
 
