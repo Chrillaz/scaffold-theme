c05b0398e34f3c9596d1dd17a50f01f0366bb398
Chrillaz <totalchriz@gmail.com>
2021-02-07 22:45:03 +0100

    hook args check

---
+++ lib/Services/Bootstrap.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Theme\Scaffold\Services;
+
+use Theme\Scaffold\Theme;
+
+use Theme\Scaffold\Providers\Facade;
+
+use Theme\Scaffold\Integrations\Integrations;
+
+class Bootstrap {
+
+  private $theme;
+
+  public function __construct ( Theme $theme ) {
+
+    $this->theme = $theme;
+
+    $theme->hooks()->addAction( 'after_setup_theme', 'setup', $this );
+    // Customizer | Settings page
+    
+    $theme->hooks()->addAction( 'wp_enqueue_scripts', 'load', $theme->assets()->context( 'public' ) );
+
+    $theme->hooks()->addAction( 'enqueue_block_editor_assets', 'load', $theme->assets()->context( 'editor' ) );
+
+    $theme->hooks()->addAction( 'script_loader_tag', 'scriptExec', $theme->assets(), 10, 2 );
+
+    $this->integrations( $theme->settings()->get( 'integrations' ) );
+  }
+
+  public function integrations ( array $integrations ) {
+
+    Integrations::create( $this->theme, $integrations );
+  }
+
+  public function setup () {
+    
+  }
+}
