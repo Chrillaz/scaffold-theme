aea9f37c58063719f11342ba96131a457d6ea3ed
Christoffer Öhman <totalchriz@gmail.com>
2021-04-16 18:52:58 +0200

    Storage Test (#13)
    * Storage Test
    
    * starting point for container tests

--- tests/test-storage.php
+++ tests/test-storage.php
@@ -6,22 +6,92 @@ use WpTheme\Scaffold\Framework\Resources\Storage;
 
 class StorageTest extends TestCase {
   
-  public function test_basic_test () {
+  private $values;
 
-    $storage = new Storage([
-      'key1' => 'value',
-      'key2' => 'value',
-      'key3' => 'value'
-    ]);
-    
-    $this->assertTrue( $storage->contains( 'key2' ) );
+  private $storage;
 
-    $this->assertFalse( $storage->get( 'key4' ) );
-    
-    $this->assertEquals( 'value', $storage->get( 'key2' ) );
-    
-    $this->assertIsArray( $storage->collect( 'key1', 'key3' ) ); 
+  protected function setUp (): void {
 
-    $this->assertIsArray( $storage->all() );
+    $this->values = [
+      'key1' => 'value 1',
+      'key2' => 'value 2',
+      'key3' => 'value 3'
+    ];
+
+    $this->storage = new Storage( $this->values );
+  }
+
+  /**
+   * @test
+   */
+  public function storageContains (): void {
+    
+    $this->assertTrue( $this->storage->contains( 'key2' ) );
+
+    $this->assertTrue( $this->storage->contains( 'value 1' ) );
+
+    $this->assertFalse( $this->storage->contains( 'key5' ) );
+
+    $this->assertFalse( $this->storage->contains( 'Not in storage value' ) );
+  }
+
+  /**
+   * @test
+   */
+  public function storageGet (): void {
+
+    $this->assertEquals( 'value 3', $this->storage->get( 'key3' ) );
+
+    $this->assertFalse( $this->storage->get( 'key4' ) );
+  }
+
+  /**
+   * @test
+   */
+  public function storageAll (): void {
+
+    $this->assertIsArray( $this->storage->all() );
+
+    $this->assertEquals( $this->values, $this->storage->all() );
+  }
+
+  /**
+   * @test
+   */
+  public function storageCollect (): void {
+
+    $collected = $this->storage->collect( 'key1', 'key3' );
+
+    $this->assertIsArray( $collected );
+
+    $this->assertCount( 2, $collected );
+  }
+
+  /**
+   * @test
+   */
+  public function storageSet (): void {
+
+    $this->storage->set( 'key4', 'value 4' );
+
+    $this->assertTrue( $this->storage->contains( 'value 4' ) );
+
+    $this->assertEquals( 'value 4', $this->storage->get( 'key4' ) );
+    
+    $this->assertCount( 4, $this->storage->all() );
+  }
+
+  /**
+   * @test
+   */
+  public function storageDelete (): void {
+    
+    $this->assertFalse( $this->storage->delete( 'key5' ) );
+
+    $this->assertTrue( $this->storage->delete( 'key3' ) );
+
+    $this->assertFalse( $this->storage->get( 'key3' ) );
+
+    $this->assertCount( 2, $this->storage->all() );
   }
 }
