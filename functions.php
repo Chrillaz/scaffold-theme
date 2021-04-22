de7c29c49164d2f81c89e336d1faac8e83df1528
Chrillaz <totalchriz@gmail.com>
2021-04-22 23:51:46 +0200

    rebase

--- functions.php
+++ functions.php
@@ -1,7 +1,3 @@
 <?php
 
-<<<<<<< HEAD
-$theme = require get_template_directory() . '/src/Framework/Bootstrap.php';
-=======
 $theme = require __DIR__ . '/src/Framework/Bootstrap.php';
->>>>>>> removal
