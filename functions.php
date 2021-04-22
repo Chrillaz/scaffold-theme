889c1c2dff19d3393c436b4610c7ffee9f1c4dca
Chrillaz <totalchriz@gmail.com>
2021-04-22 23:26:39 +0200

    removal

--- functions.php
+++ functions.php
@@ -1,3 +1,7 @@
 <?php
 
+<<<<<<< HEAD
 $theme = require get_template_directory() . '/src/Framework/Bootstrap.php';
+=======
+$theme = require __DIR__ . '/src/Framework/Bootstrap.php';
+>>>>>>> removal
