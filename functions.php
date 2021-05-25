0b6ffc1fa05f26849c818cd8c60b3ab0d54d8b15
Chrillaz <totalchriz@gmail.com>
2021-05-25 06:58:46 +0200

    fix

--- functions.php
+++ functions.php
@@ -8,9 +8,3 @@ function Theme () {
 
   return $app->make( \Scaffold\Theme\Theme::class );
 }
-
-$cache = Theme()->container()->make(\Scaffold\Essentials\Contracts\CacheInterface::class);
-
-$cache->set( 'test', 'hej', 'testgroup' );
-
-var_dump('<pre>', $cache, '</pre>');
